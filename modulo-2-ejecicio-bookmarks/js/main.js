101a80837f3939da18c2227e454d2e0859138a10
Natalia <atvnataliajn@gmail.com>
2021-11-30 11:06:50 +0100

    add exercise

---
+++ modulo-2-ejecicio-bookmarks/js/main.js
@@ -0,0 +1,19 @@
+"use strict"
+
+/* Dia 1*/
+/* 1- Prueba mostrar formulario nuevo bookmark */
+/* 2- Prueba mostrar menu */
+/* 3- Añadir informacion de los enlaces desde JS
+    3.1 Traer el elemento html
+    3.2 Variable que contenga toda la información del li por cada uno de los enlaces
+    3.3 Añadir esa info con el innerHTML por cada uno de los enlaces
+*/
+
+const form= document.querySelector('.collapsed');
+form.classList.remove('collapsed');
+
+const section= document.querySelector('.data-actions__add');
+section.classList.remove('hidden');
+
+let link= document.querySelector('.data__listitem');
+
